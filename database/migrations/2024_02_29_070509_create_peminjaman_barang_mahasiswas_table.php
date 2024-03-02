f4187ea494a91dfd9f56a94a924bcbecd15187c4
callmeeve <wididiky38@gmail.com>
2024-03-02 22:30:14 +0700

    baru lagi

--- database/migrations/2024_02_29_070509_create_peminjaman_barang_mahasiswas_table.php
+++ database/migrations/2024_02_29_070509_create_peminjaman_barang_mahasiswas_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->string('prodi');
             $table->string('jurusan');
             $table->unsignedBigInteger('barang_id');
-            $table->foreign('barang_id')->references('id')->on('barang_lab')->onDelete('cascade');
+            $table->foreign('barang_id')->references('id_barang')->on('barang_lab')->onDelete('cascade');
             $table->integer('jumlah');
             $table->enum('status', ['menunggu_persetujuan_dosen', 'menunggu_persetujuan_teknisi', 'sudah_tervalidasi', 'ditolak']);
             $table->unsignedBigInteger('dosen_approver_id')->nullable();
