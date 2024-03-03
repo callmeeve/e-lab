d7ff41d57b21e7d0f93e3813a0f0cbbc6866afa8
callmeeve <wididiky38@gmail.com>
2024-03-03 22:03:20 +0700

    register login baru

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -21,11 +21,11 @@
 
                     <form id="formAuthentication" class="mb-3" action="{{ route('login') }}" method="POST">
                         @csrf
-                        <div class="mb-3">
+                        <div class="mb-4">
                             <label for="email" class="form-label">Email atau Username</label>
                             <input type="text" class="form-control" id="email" name="email" placeholder="Enter your email or username" autofocus />
                         </div>
-                        <div class="mb-3 form-password-toggle">
+                        <div class="mb-5 form-password-toggle">
                             <div class="d-flex justify-content-between">
                                 <label class="form-label" for="password">Password</label>
                                 <a href="/">
@@ -41,13 +41,6 @@
                             <button class="btn btn-primary d-grid w-100" type="submit">Sign in</button>
                         </div>
                     </form>
-
-                    <p class="text-center">
-                        <span>Belum punya akun?</span>
-                        <a href="">
-                            <span>Buat Akun</span>
-                        </a>
-                    </p>
                 </div>
             </div>
             <!-- /Register -->
--- resources/views/mahasiswa/register.blade.php
+++ resources/views/mahasiswa/register.blade.php
@@ -1,6 +1,6 @@
-@extends('layout.log')
+@extends('layout.regMahasiswa')
 
-@section('isi-login')
+@section('isi-registermahasiswa')
     <!-- Basic Layout -->
     <div class="container-xxl">
         <div class="authentication-wrapper authentication-basic container-p-y">
@@ -9,31 +9,34 @@
                 <div class="card">
                     <div class="card-body">
                         <!-- Logo -->
-                        <div class="app-brand justify-content-center">
+                        <div class="app-brand justify-content-center mb-2">
                             <a href="#" class="app-brand-link gap-2">
                                 <span class="app-brand-logo demo">
-                                    <!-- Your logo SVG or image here -->
+                                    <img src="{{ asset('img/poliwangi.png') }}" alt="Brand Logo" />
                                 </span>
-                                <span class="app-brand-text demo text-body fw-bold">Your App Name</span>
+                                <span class="app-brand-text demo menu-text fw-bold ms-2">E-Lab</span>
                             </a>
                         </div>
                         <!-- /Logo -->
-                        <h4 class="mb-2">Register Mahasiswa</h4>
+                        <p class="text-center mb-2">Mohon untuk mahasiswa agar register akun terlebih dahulu</p>
 
-                        <form method="POST" action="{{route('mahasiswa.registerAkun')}}" class="mb-3">
+                        <form method="POST" action="{{ route('mahasiswa.registerAkun') }}" class="mb-3">
                             @csrf
                             <div class="mb-3">
                                 <label for="username" class="form-label">Username</label>
-                                <input type="text" class="form-control" id="username" name="username" placeholder="Enter your username" autofocus />
+                                <input type="text" class="form-control" id="username" name="username"
+                                    placeholder="Enter your username" autofocus />
                             </div>
                             <div class="mb-3">
                                 <label for="email" class="form-label">Email</label>
-                                <input type="text" class="form-control" id="email" name="email" placeholder="Enter your email" />
+                                <input type="text" class="form-control" id="email" name="email"
+                                    placeholder="Enter your email" />
                             </div>
                             <div class="mb-3 form-password-toggle">
                                 <label class="form-label" for="password">Password</label>
                                 <div class="input-group input-group-merge">
-                                    <input type="password" id="password" class="form-control" name="password" placeholder="Enter your password" />
+                                    <input type="password" id="password" class="form-control" name="password"
+                                        placeholder="Enter your password" />
                                     <span class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
                                 </div>
                             </div>
@@ -42,9 +45,9 @@
                         </form>
 
                         <p class="text-center">
-                            <span>Already have an account?</span>
-                            <a href="/">
-                                <span>Sign in instead</span>
+                            <span>Sudah punya akun?</span>
+                            <a href="{{ route('login') }}">
+                                <span>Login</span>
                             </a>
                         </p>
                     </div>
