ddd72bb3aeb680bf58654c0531060c11ac5710a2
callmeeve <wididiky38@gmail.com>
2024-03-09 16:11:22 +0700

    landing page done

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -29,8 +29,8 @@
                 aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                    <li class="nav-item"><a class="nav-link text-dark" href="#!">Login</a></li>
-                    <li class="nav-item"><a class="nav-link text-dark" href="#!">Register</a></li>
+                    <li class="nav-item"><a class="nav-link text-dark" href="{{route('login')}}">Login</a></li>
+                    <li class="nav-item"><a class="nav-link text-dark" href="{{route('mahasiswa.registerAkun')}}">Register</a></li>
                 </ul>
             </div>
         </div>
@@ -41,97 +41,53 @@
             <div class="row gx-5 justify-content-center">
                 <div class="col-lg-6">
                     <div class="text-center my-5">
-                        <h1 class="display-5 fw-bolder text-white mb-2">Present your business in a whole new way</h1>
-                        <p class="lead text-white-50 mb-4">Quickly design and customize responsive mobile-first sites
-                            with Bootstrap, the world’s most popular front-end open source toolkit!</p>
+                        <h1 class="display-5 fw-bolder text-white mb-2">Temukan Peminjaman Alat Lab Poliwangi</h1>
+                        <p class="lead text-white-50 mb-4">Dapatkan pinjaman alat laboratorium berkualitas tinggi untuk
+                            kebutuhan eksperimen dan penelitian Anda dengan mudah.</p>
                         <div class="d-grid gap-3 d-sm-flex justify-content-sm-center">
-                            <a class="btn btn-primary btn-lg px-4 me-sm-3" href="#features">Get Started</a>
-                            <a class="btn btn-outline-light btn-lg px-4" href="#!">Learn More</a>
+                            <a class="btn btn-primary btn-lg px-4 me-sm-3" href="#features">Mulai Sekarang</a>
+                            <a class="btn btn-outline-light btn-lg px-4" href="#!">Pelajari Lebih Lanjut</a>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </header>
-    <!-- Features section-->
     <section class="py-5 border-bottom" id="features">
         <div class="container px-5 my-5">
             <div class="row gx-5">
                 <div class="col-lg-4 mb-5 mb-lg-0">
                     <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i
                             class="bi bi-collection"></i></div>
-                    <h2 class="h4 fw-bolder">Featured title</h2>
-                    <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another
-                        sentence and probably just keep going until we run out of words.</p>
+                    <h2 class="h4 fw-bold text-dark">Pilihan Alat Lab yang Luas</h2>
+                    <p>Temukan berbagai macam peralatan laboratorium berkualitas tinggi yang sesuai dengan kebutuhan
+                        eksperimen dan penelitian Anda di Poliwangi.</p>
                     <a class="text-decoration-none" href="#!">
-                        Call to action
+                        Lihat Lebih Lanjut
                         <i class="bi bi-arrow-right"></i>
                     </a>
                 </div>
                 <div class="col-lg-4 mb-5 mb-lg-0">
                     <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-building"></i>
                     </div>
-                    <h2 class="h4 fw-bolder">Featured title</h2>
-                    <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another
-                        sentence and probably just keep going until we run out of words.</p>
+                    <h2 class="h4 fw-bold text-dark">Fasilitas Laboratorium Modern</h2>
+                    <p>Manfaatkan fasilitas laboratorium terbaik dengan teknologi mutakhir untuk mendukung riset dan
+                        eksperimen Anda di Poliwangi.</p>
                     <a class="text-decoration-none" href="#!">
-                        Call to action
+                        Telusuri Lebih Lanjut
                         <i class="bi bi-arrow-right"></i>
                     </a>
                 </div>
                 <div class="col-lg-4">
                     <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-toggles2"></i>
                     </div>
-                    <h2 class="h4 fw-bolder">Featured title</h2>
-                    <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another
-                        sentence and probably just keep going until we run out of words.</p>
+                    <h2 class="h4 fw-bold text-dark">Proses Peminjaman yang Mudah</h2>
+                    <p>Nikmati proses peminjaman yang cepat dan mudah untuk alat laboratorium Poliwangi, sehingga Anda
+                        bisa fokus pada riset dan eksperimen Anda.</p>
                     <a class="text-decoration-none" href="#!">
-                        Call to action
+                        Pelajari Selengkapnya
                         <i class="bi bi-arrow-right"></i>
                     </a>
-                </div>
-            </div>
-        </div>
-    </section>
-    <!-- Testimonials section-->
-    <section class="py-5 border-bottom">
-        <div class="container px-5 my-5 px-5">
-            <div class="text-center mb-5">
-                <h2 class="fw-bolder">Customer testimonials</h2>
-                <p class="lead mb-0">Our customers love working with us</p>
-            </div>
-            <div class="row gx-5 justify-content-center">
-                <div class="col-lg-6">
-                    <!-- Testimonial 1-->
-                    <div class="card mb-4">
-                        <div class="card-body p-4">
-                            <div class="d-flex">
-                                <div class="flex-shrink-0"><i class="bi bi-chat-right-quote-fill text-primary fs-1"></i>
-                                </div>
-                                <div class="ms-4">
-                                    <p class="mb-1">Thank you for putting together such a great product. We loved
-                                        working with you and the whole team, and we will be recommending you to others!
-                                    </p>
-                                    <div class="small text-muted">- Client Name, Location</div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- Testimonial 2-->
-                    <div class="card">
-                        <div class="card-body p-4">
-                            <div class="d-flex">
-                                <div class="flex-shrink-0"><i
-                                        class="bi bi-chat-right-quote-fill text-primary fs-1"></i></div>
-                                <div class="ms-4">
-                                    <p class="mb-1">The whole team was a huge help with putting things together for
-                                        our company and brand. We will be hiring them again in the near future for
-                                        additional work!</p>
-                                    <div class="small text-muted">- Client Name, Location</div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
                 </div>
             </div>
         </div>
@@ -144,18 +100,15 @@
                         <div class="row align-items-center">
                             <div class="offset-xl-1 col-xl-5 col-md-6 col-12">
                                 <div>
-                                    <h2 class="h1 text-white mb-3">Let’s find the right course for you!</h2>
-                                    <p class="text-white fs-4">…and achieve their learning goals. With our expert
-                                        tutors, your goals are
-                                        closer
-                                        than ever!</p>
-                                    <button class="btn btn-dark">Start learning</button>
+                                    <h2 class="h1 text-white mb-3 fw-bold">Temukan layanan peminjaman alat lab di Poliwangi!</h2>
+                                    <p class="text-white fs-5">Dengan pelayanan kami, pengadaan alat lab menjadi lebih mudah dan efisien!</p>
+                                    <button class="btn btn-dark">Mulai pinjam</button>
                                 </div>
                             </div>
                             <div class="col-xl-6 col-md-6 col-12">
                                 <div class="text-center">
-                                    <img src="{{asset("img/icons/gear.png")}}" alt="learning"
-                                        class="img-fluid" width="200" height="200">
+                                    <img src="{{ asset('img/icons/gear.png') }}" alt="learning" class="img-fluid"
+                                        width="200" height="200">
                                 </div>
                             </div>
                         </div>
@@ -167,7 +120,7 @@
     <!-- Footer-->
     <footer class="py-5 bg-white">
         <div class="container px-5">
-            <p class="m-0 text-center text-dark">Copyright &copy; Your Website 2023</p>
+            <p class="m-0 text-center text-dark">Copyright &copy; E-Lab Poliwangi 2024</p>
         </div>
     </footer>
     <!-- Bootstrap core JS-->
